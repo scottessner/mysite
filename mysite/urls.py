403126821cbf4aa7b73a23fa8fa9d20454f8d09b
scottessner <scott.essner@gmail.com>
2015-11-05 17:43:40 -0600

    End of part 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls))
 ]
